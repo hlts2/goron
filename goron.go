5d95cb3db064a5111905836a25a45c49ba160ab2
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2018-09-05 14:34:42 +0900

    added: processing to set time spec

--- goron.go
+++ goron.go
@@ -2,10 +2,20 @@ package goron
 
 import (
 	"sync"
+	"time"
 )
 
 // DefaultJobCount is the number of job
 const DefaultJobCount int = 1000
+
+// const
+const (
+	Minute = iota
+	Hour
+	Day
+	Month
+	Week
+)
 
 type (
 	// JobHandler represents job handler type for goron
@@ -14,8 +24,8 @@ type (
 	// Job represents job info
 	Job struct {
 		g       *goron
-		Spec    string
-		Handler JobHandler
+		spec    []string
+		handler JobHandler
 		finish  chan bool
 	}
 
@@ -34,12 +44,13 @@ type (
 		Minute(spec string) Goron
 		With(handlers ...JobHandler)
 		AddJob(spec string, handlers ...JobHandler)
+		JobCount() int
 		Run()
 	}
 
 	goron struct {
 		mu   *sync.Mutex
-		spec string
+		spec []string
 		jobs Jobs
 	}
 )
@@ -49,61 +60,91 @@ func New() Goron {
 	return &goron{
 		mu:   new(sync.Mutex),
 		jobs: make(Jobs, 0, DefaultJobCount),
+		spec: initSpec(),
 	}
 }
 
-func (g *goron) Week(spec string) Goron {
-	return g
+func initSpec() []string {
+	return []string{"*", "*", "*", "*"}
 }
 
-func (g *goron) Month(spec string) Goron {
-	return g
-}
+func (g *goron) Minute(spec string) Goron {
+	if len(g.jobs) < Minute {
+		return g
+	}
 
-func (g *goron) Day(spec string) Goron {
+	g.spec[Minute] = spec
 	return g
 }
 
 func (g *goron) Hour(spec string) Goron {
+	if len(g.jobs) < Hour {
+		return g
+	}
+
+	g.spec[Hour] = spec
 	return g
 }
 
-func (g *goron) Minute(spec string) Goron {
+func (g *goron) Day(spec string) Goron {
+	if len(g.jobs) < Day {
+		return g
+	}
+
+	g.spec[Day] = spec
+	return g
+}
+
+func (g *goron) Month(spec string) Goron {
+	if len(g.jobs) < Month {
+		return g
+	}
+
+	g.spec[Month] = spec
+	return g
+}
+
+func (g *goron) Week(spec string) Goron {
+	if len(g.jobs) < Week {
+		return g
+	}
+
+	g.spec[Week] = spec
 	return g
 }
 
 func (g *goron) With(handlers ...JobHandler) {
-	g.mu.Lock()
 	g.addJob(g.spec, handlers...)
-	g.spec = ""
-	g.mu.Unlock()
 }
 
 func (g *goron) AddJob(spec string, handlers ...JobHandler) {
-	g.mu.Lock()
-	g.addJob(spec, handlers...)
-	g.spec = ""
-	g.mu.Unlock()
+	g.addJob(g.spec, handlers...)
 }
 
-func (g *goron) addJob(spec string, handlers ...JobHandler) {
+func (g *goron) addJob(spec []string, handlers ...JobHandler) {
 	for _, handler := range handlers {
 		g.jobs = append(g.jobs, Job{
-			Handler: handler,
-			Spec:    spec,
-			finish:  make(chan bool),
 			g:       g,
+			spec:    spec,
+			handler: handler,
+			finish:  make(chan bool),
 		})
 	}
 }
 
+func (g *goron) JobCount() int {
+	return len(g.jobs)
+}
+
 func (g *goron) Run() {
+	t := time.Now()
+
 	for _, job := range g.jobs {
-		go job.run()
+		go job.run(t)
 	}
 }
 
-func (j Job) run() {
+func (j Job) run(t time.Time) {
 END_LOOP:
 	for {
 		select {
